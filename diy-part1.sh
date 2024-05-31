4065a215963f2afabd9b8fca6de33490bea23f70
lixiaowei <267199@qq.com>
2024-06-01 03:56:25 +0800

    mt7621first

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,16 +18,22 @@
 #git clone https://github.com/MrH723/openwrt-packages.git package/openwrt-packages
 #sed -i '$a src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
 
+# Add a feed source
+echo 'src-git openwrtpackages https://github.com/openwrt/packages' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/dmulxw/helloworldopenwrt' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
 # fix upx ucl 不存在;
-apt install subversion -y
-svn export --force https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
-svn export --force https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
+#apt install subversion -y
+#svn export --force https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
+#svn export --force https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
 
-n=`sed -n '/^tools-y +=/{p;q}' tools/Makefile`
-sed -i "/${n}/i\tools-y += ucl upx " tools/Makefile
+#n=`sed -n '/^tools-y +=/{p;q}' tools/Makefile`
+#sed -i "/${n}/i\tools-y += ucl upx " tools/Makefile
 
-n=`sed -n '/^$(curdir)/{p;q}' tools/Makefile`
-sed -i '1,/${n}/ {/# builddir dependencies/a\
-$(curdir)/upx/compile := $(curdir)/ucl/compile
-}' tools/Makefile
+#n=`sed -n '/^$(curdir)/{p;q}' tools/Makefile`
+#sed -i '1,/${n}/ {/# builddir dependencies/a\
+#$(curdir)/upx/compile := $(curdir)/ucl/compile
+#}' tools/Makefile
 
